1a72e45e657619823edbf8301462da12e8b47ba5
Apertyotis <1947517504@qq.com>
2026-01-20 02:59:53 +0800

    Fix incorrect rotation speed usage

--- src/main/java/net/apertyotis/createandesiteabound/mixin/create/MixerBlockEntityMixin.java
+++ src/main/java/net/apertyotis/createandesiteabound/mixin/create/MixerBlockEntityMixin.java
@@ -1,5 +1,7 @@
 package net.apertyotis.createandesiteabound.mixin.create;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.simibubi.create.content.kinetics.mixer.MechanicalMixerBlockEntity;
 import com.simibubi.create.content.processing.basin.BasinBlockEntity;
 import com.simibubi.create.content.processing.basin.BasinOperatingBlockEntity;
@@ -12,6 +14,7 @@ import net.minecraft.sounds.SoundSource;
 import net.minecraft.util.Mth;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -29,20 +32,24 @@ public abstract class MixerBlockEntityMixin extends BasinOperatingBlockEntity {
     @Shadow
     public int processingTicks;
 
-    @Shadow
-    public boolean running;
-
     // 创建空构造函数来通过编译器语法检查，没有实际作用
     public MixerBlockEntityMixin(BlockEntityType<?> typeIn, BlockPos pos, BlockState state) {
         super(typeIn, pos, state);
     }
 
-    // 提前计算配方时间，一方面替代了目标原先计算方法，一方面减少了1gt机器延迟
-    @Inject(method = "tick", at = @At("HEAD"))
-    private void onTick(CallbackInfo ci) {
-        if (level == null || level.isClientSide) return;
+    // 重写计算配方时间逻辑，一方面替代了目标原先计算方法，一方面减少了1gt机器延迟
+    @WrapOperation(
+            method = "tick",
+            at = @At(
+                    value = "FIELD",
+                    target = "Lcom/simibubi/create/content/kinetics/mixer/MechanicalMixerBlockEntity;processingTicks:I",
+                    opcode = Opcodes.GETFIELD,
+                    ordinal = 0
+            )
+    )
+    private int modifyProcessingTicks(MechanicalMixerBlockEntity instance, Operation<Integer> original) {
         // 计算配方时间
-        if (running && runningTicks == 20 && processingTicks < 0) {
+        if (processingTicks < 0) {
             float recipeSpeed = 1;
             if (currentRecipe instanceof ProcessingRecipe) {
                 int t = ((ProcessingRecipe<?>) currentRecipe).getProcessingDuration();
@@ -51,17 +58,18 @@ public abstract class MixerBlockEntityMixin extends BasinOperatingBlockEntity {
             }
 
             // 调整了不恰当的log2函数使用，并再减少1gt时间
-            processingTicks = Mth.clamp((Mth.ceillog2((int) (512 / speed))) * Mth.ceil(recipeSpeed * 15), 1, 512);
+            processingTicks = Mth.clamp((Mth.ceillog2((int) (512 / Math.abs(getSpeed())))) * Mth.ceil(recipeSpeed * 15), 1, 512);
 
             // 后续为目标原先服务端播放音效逻辑
             Optional<BasinBlockEntity> basin = getBasin();
-            if (basin.isPresent()) {
+            if (level != null && basin.isPresent()) {
                 Couple<SmartFluidTankBehaviour> tanks = basin.get().getTanks();
                 if (!tanks.getFirst().isEmpty() || !tanks.getSecond().isEmpty())
                     level.playSound(null, worldPosition, SoundEvents.BUBBLE_COLUMN_WHIRLPOOL_AMBIENT,
                             SoundSource.BLOCKS, .75f, speed < 65 ? .75f : 1.5f);
             }
         }
+        return processingTicks;
     }
 
     // 增加1tick启动延迟，来抵消抬头过快的bug
