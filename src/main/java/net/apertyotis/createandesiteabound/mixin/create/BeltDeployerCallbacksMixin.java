7881acef71fde534acc0784d70bcc1757c69d777
Apertyotis <1947517504@qq.com>
2026-01-26 01:42:02 +0800

    Fix a bug where the Deployer would process the first item in a stack 1 tick faster

--- src/main/java/net/apertyotis/createandesiteabound/mixin/create/BeltDeployerCallbacksMixin.java
+++ src/main/java/net/apertyotis/createandesiteabound/mixin/create/BeltDeployerCallbacksMixin.java
@@ -10,6 +10,22 @@ import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(value = BeltDeployerCallbacks.class, remap = false)
 public abstract class BeltDeployerCallbacksMixin {
+    // 让机械手在传送带和置物台上时不再跳过 WAITING
+    @WrapOperation(
+            method = "onItemReceived",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lcom/simibubi/create/content/kinetics/deployer/DeployerBlockEntity;start()V"
+            )
+    )
+    private static void waitStart_1(DeployerBlockEntity instance, Operation<Void> original) {
+        DeployerBlockEntityAccessor accessor = (DeployerBlockEntityAccessor) instance;
+        if (accessor.getTimer() <= -1000) {
+            original.call(instance);
+        }
+    }
+
+    // 让机械手在置物台上时不再跳过 WAITING
     @WrapOperation(
             method = "whenItemHeld",
             at = @At(
@@ -17,8 +33,7 @@ public abstract class BeltDeployerCallbacksMixin {
                     target = "Lcom/simibubi/create/content/kinetics/deployer/DeployerBlockEntity;start()V"
             )
     )
-    private static void waitStart(DeployerBlockEntity instance, Operation<Void> original) {
-        // 让机械手在传送带上时不再跳过WAIT
+    private static void waitStart_2(DeployerBlockEntity instance, Operation<Void> original) {
         DeployerBlockEntityAccessor accessor = (DeployerBlockEntityAccessor) instance;
         if (accessor.getTimer() <= -1000) {
             original.call(instance);
