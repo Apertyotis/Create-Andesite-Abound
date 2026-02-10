8ae4b488daac0d97352e1cd763dee8cfa9489f51
Apertyotis <1947517504@qq.com>
2026-02-10 17:13:47 +0800

    Fix client-side desync of contraption

--- src/main/java/net/apertyotis/createandesiteabound/mixin/create/ControlledContraptionEntityMixin.java
+++ src/main/java/net/apertyotis/createandesiteabound/mixin/create/ControlledContraptionEntityMixin.java
@@ -10,21 +10,16 @@ import net.minecraft.nbt.Tag;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = ControlledContraptionEntity.class, remap = false)
 public abstract class ControlledContraptionEntityMixin {
     @Shadow
     protected BlockPos controllerPos;
 
-    @Inject(method = "readAdditional", at = @At("TAIL"))
-    private void redirectReadControllerPos(CompoundTag compound, boolean spawnPacket, CallbackInfo ci) {
-        if (compound.contains("ControllerPos")) {
-            controllerPos = NbtUtils.readBlockPos(compound.getCompound("ControllerPos"));
-        }
-    }
-
+    /**
+     * 修复运动结构会在客户端消失的问题，详见 Create PR <a href="https://github.com/Creators-of-Create/Create/pull/9824">#8492</a><br>
+     * 这个修复大概率是有效的，但是目前无论原修复者还是 Create 开发者都无法确定它为什么有效
+     */
     @WrapOperation(
             method = "writeAdditional",
             at = @At(
@@ -34,6 +29,6 @@ public abstract class ControlledContraptionEntityMixin {
             remap = true
     )
     private Tag redirectWriteControllerPos(CompoundTag instance, String p_128366_, Tag p_128367_, Operation<Tag> original) {
-        return original.call(instance, "ControllerPos", NbtUtils.writeBlockPos(controllerPos));
+        return original.call(instance, "Controller", NbtUtils.writeBlockPos(controllerPos));
     }
 }
