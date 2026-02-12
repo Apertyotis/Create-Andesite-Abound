0bd844640c063f541b31a887cd19ad0f9861ee2f
Apertyotis <1947517504@qq.com>
2026-02-13 07:05:23 +0800

    Fix the issue where Adjustable Chain Gearshifts crash the game with specific redstone inputs

---
+++ src/main/java/net/apertyotis/createandesiteabound/mixin/create/RotationPropagatorMixin.java
@@ -0,0 +1,35 @@
+package net.apertyotis.createandesiteabound.mixin.create;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
+import com.simibubi.create.content.kinetics.RotationPropagator;
+import com.simibubi.create.content.kinetics.base.KineticBlockEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(value = RotationPropagator.class, remap = false)
+public abstract class RotationPropagatorMixin {
+    /**
+     * 修复*已屏蔽的禁忌知识*导致坏档的问题<br>
+     * 详见 Create PR <a href="https://github.com/Creators-of-Create/Create/pull/9803">#9803</a>
+     */
+    @WrapOperation(
+            method = "propagateNewSource",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lcom/simibubi/create/content/kinetics/base/KineticBlockEntity;getTheoreticalSpeed()F",
+                    ordinal = 2
+            )
+    )
+    private static float redirectRPM(
+            KineticBlockEntity instance, Operation<Float> original,
+            @Local(name = "newSpeed") float newSpeed)
+    {
+        float speed = original.call(instance);
+        if (Math.abs(speed - newSpeed) <= 1e-5f)
+            return newSpeed;
+        else
+            return speed;
+    }
+}
